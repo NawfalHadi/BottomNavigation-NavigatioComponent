8618f1b3ba9ec8c7c13acfdd07bd12137d26905b
Nawfal Hadi <nawfalhdi@gmail.com>
2022-09-06 23:03:22 +0700

    Bottom Navigation

--- app/src/main/java/com/thatnawfal/binarc3t4/MainActivity.kt
+++ app/src/main/java/com/thatnawfal/binarc3t4/MainActivity.kt
@@ -3,14 +3,30 @@ package com.thatnawfal.binarc3t4
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navContainer) as NavHostFragment
+        navController = navHostFragment.navController
 
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        setupActionBarWithNavController(navController)
+        bottomNavigationView.setupWithNavController(navController)
+    }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
